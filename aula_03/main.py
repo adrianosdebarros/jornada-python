56fff5326cd20ac2ffebd8550d167d4db822e585
adrianosdebarros <adriano.sdebarros@gmail.com>
2025-09-25 00:02:23 -0300

    Iniciando aula 3

---
+++ aula_03/main.py
@@ -0,0 +1,19 @@
+# print('--- Entendendo o que é debugar ---\n')
+
+# print('Primeiro comando')
+# print('Segundo comando')
+# print('Terceiro comando')
+# x = 3
+# print(x)
+
+print('--- Controle de fluxo ---\n')
+
+# numero = int(input("Insira um número e lhe direi se ele é positivo ou negativo: "))
+
+# if numero < 0:
+#     print(f'O número que você digitou foi {numero} e ele é negativo.')
+# elif numero == 0: 
+#     print(f'O número que você digitou foi {numero} e ele é nulo.')
+# else:
+#     print(f'O número que você digitou foi {numero} e ele é positivo.')
+
