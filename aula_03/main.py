ef5d562733f2cf98dc5a86a0028fba5866aa6526
adrianosdebarros <adriano.sdebarros@gmail.com>
2025-09-28 22:26:13 -0300

    Introdução a debug, if, for, while, listas e dicionarios

--- aula_03/main.py
+++ aula_03/main.py
@@ -6,7 +6,7 @@
 # x = 3
 # print(x)
 
-print('--- Controle de fluxo ---\n')
+# print('--- Controle de fluxo ---\n')
 
 # numero = int(input("Insira um número e lhe direi se ele é positivo ou negativo: "))
 
@@ -17,3 +17,27 @@ print('--- Controle de fluxo ---\n')
 # else:
 #     print(f'O número que você digitou foi {numero} e ele é positivo.')
 
+# 1°)
+
+# temperatura = float(input('Temperatura atual: '))
+
+# if temperatura < 18:
+#     print(f'A temperatura atual de {temperatura} graus é considerada baixa.')
+# elif temperatura >= 18 and temperatura <= 26:
+#     print(f'A temperatura atual de {temperatura} graus está no nível normal.')
+# else:
+#     print(f'A temperatura atual de {temperatura} graus é considerada alta.')
+
+
+# lista_de_alunos = ["adriano", "taires", "alice"]
+
+# for aluno in lista_de_alunos:
+#     print(aluno)
+
+import time
+
+condicao = True
+
+while condicao:
+    print("Execute minha ETL")
+    time.sleep(5)
